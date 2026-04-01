b833894f64ed5281b49f4e5f39cca6b8502e96ac
fabioalmeida08 <fabioalmeida0896@gmail.com>
2026-04-01 15:21:14 -0300

    refactor: removed draw file and split in another files the draw functions

--- src/utils/color_utils.c
+++ src/utils/color_utils.c
@@ -16,3 +16,13 @@ int color(int r, int g, int b)
 {
     return ((r << 16) | (g << 8) | b);
 }
+
+void    put_pixel(t_img *img, int x, int y, int color)
+{
+    char    *dst;
+
+    if (x < 0 || x >= WIN_W || y < 0 || y >= WIN_H)
+        return ;
+    dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+    *(unsigned int *)dst = color;
+}
