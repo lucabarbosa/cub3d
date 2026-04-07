7b4c82195c1cdf976e4929154bcd373fec398c10
Luca Barbosa <lucabento.luca@gmail.com>
2026-04-07 00:33:06 -0300

    Flood fill implemented and checking valid maps

--- src/parser/validate_map.c
+++ src/parser/validate_map.c
@@ -6,15 +6,18 @@
 /*   By: lbento <lbento@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/06 00:02:04 by lbento            #+#    #+#             */
-/*   Updated: 2026/04/06 02:41:03 by lbento           ###   ########.fr       */
+/*   Updated: 2026/04/07 00:31:36 by lbento           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/parser.h"
 #include "../../includes/cub3d.h"
 
+void	validate_map(t_file *file, t_gc **collector);
 static int	check_chars(t_file *file);
 static int	check_player(t_file *file, t_gc **collector);
+static int	secure_map(t_file *file, t_gc **collector);
+static int	flood_fill(char **map, int row, int col);
 
 void	validate_map(t_file *file, t_gc **collector)
 {
@@ -26,8 +29,8 @@ void	validate_map(t_file *file, t_gc **collector)
 		print_error(7, collector);
 	if (!check_player(file, collector))
 		print_error(8, collector);
-	// if (!secure_map(file, collector))
-	// 	print_error(9, collector);
+	if (!secure_map(file, collector))
+		print_error(9, collector);
 }
 
 static int	check_chars(t_file *file)
@@ -79,6 +82,61 @@ static int	check_player(t_file *file, t_gc **collector)
 		i++;
 	}
 	if (!file->player)
+		return (0);
+	return (1);
+}
+
+static int	secure_map(t_file *file, t_gc **collector)
+{
+	int	i;
+	char	**copied_map;
+
+	i = 0;
+	while (file->map[i])
+		i++;
+	copied_map = gc_malloc(collector, sizeof(char *) * (i + 1));
+	if (!copied_map)
+		print_error(10, collector);
+	i = 0;
+	while (file->map[i])
+	{
+		copied_map[i] = ft_strdup(file->map[i], collector);
+		if (!copied_map[i])
+			print_error(10, collector);
+		i++;
+	}
+	copied_map[i] = NULL;
+	if (!flood_fill(copied_map, file->player_row, file->player_col))
+		return (0);
+	gc_free(collector, copied_map);
+	return (1);
+}
+
+static int	flood_fill(char **map, int row, int col)
+{
+	int	len;
+	int	len_line;
+
+	len = 0;
+	while (map[len])
+	len++;
+	if (row < 0 || row >= len)
+		return (0);
+	len_line = ft_strlen(map[row]);
+	if (col < 0 || col >= len_line)
+		return (0);
+	if (map[row][col] == ' ' || map[row][col] == '\0' || map[row][col] == '\n')
+		return (0);
+	if (map[row][col] == '1' || map[row][col] == 'V')
+		return (1);
+	map[row][col] = 'V';
+	if (!flood_fill(map, row - 1, col))
+		return (0);
+	if (!flood_fill(map, row + 1, col))
+		return (0);
+	if (!flood_fill(map, row, col - 1))
+		return (0);
+	if (!flood_fill(map, row, col + 1))
 		return (0);
 	return (1);
 }
